60c3939c23aa5af3fe779966cc4fa4328ec6dc2d
gizyukd <gizyuk.d@gmail.com>
2021-11-22 22:41:08 +0300

    Done AddRemoveTest

--- src/test/java/AddRemoveTest.java
+++ src/test/java/AddRemoveTest.java
@@ -1,2 +1,26 @@
-public class AddRemoveTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class AddRemoveTest extends BaseTest{
+    String deleteButtonLocator = "[onclick='deleteElement()']";
+
+    @Test
+    public void addRemoveElements(){
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement addButton = driver.findElement(By.cssSelector("[onclick='addElement()']"));
+        addButton.click();
+        addButton.click();
+
+        List<WebElement> deleteButtons = driver.findElements(By.cssSelector(deleteButtonLocator));
+        assertEquals(deleteButtons.size(), 2, "Кол-во эл-ов != 2");
+
+        deleteButtons.get(1).click();
+        deleteButtons = driver.findElements(By.cssSelector(deleteButtonLocator));
+        assertEquals(deleteButtons.size(), 1, "Кол-во эл-ов != 1");
+    }
 }
